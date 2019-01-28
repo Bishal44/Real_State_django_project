c697e4b2d2c53c87c05f592804e325654c37e749
Bishal <bhattaraibishal704@gmail.com>
2019-01-28 08:41:38 +0545

    static file managed

--- btre/settings.py
+++ btre/settings.py
@@ -117,5 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+#after running collectstatic collect all admin our static file at static folder
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIR=[
+    os.path.join(BASE_DIR,'btre/static')
+]
