b59ad20a987b4d40095c958ef5c1229b190fa6cc
Bishal <bhattaraibishal704@gmail.com>
2019-02-03 17:37:33 +0545

    all media done

--- btre/urls.py
+++ btre/urls.py
@@ -2,8 +2,13 @@
 from django.contrib import admin
 from django.urls import path,include
 
+#for media file
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',include('pages.urls')),
     path('listing/',include('listings.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# +static is for image to show
