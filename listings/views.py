8282888685897a94a738230ebc47d8f81307523d
Bishal <bhattaraibishal704@gmail.com>
2019-02-07 10:02:55 +0545

    pagination,filter,order by date

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,18 @@
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.shortcuts import render
 from .models import Listing
 
 def index(request):
-    listing=Listing.objects.all();
+    listing=Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    #for paginator case
+    paginator=Paginator(listing,3)
+    page=request.GET.get('page')
+    page_listed=paginator.get_page(page)
+
     context={
-        'listings':listing
+        'listings':page_listed
+        #'listings':listing if no paginator there
     }
     return render(request,'listings/listings.html',context)
 def listing(request,listing_id):
