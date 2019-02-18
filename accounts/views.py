e502ae865200329f7894cc8baf03c4f7ee934659
Bishal <bhattaraibishal704@gmail.com>
2019-02-18 17:09:47 +0545

    register done

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def login(request):
-    return render(request,'accounts/login.html')
-
+    if request.method == 'POST':
+        pass
+     #register user
+    else:
+        return render(request,'accounts/login.html')
 
 def register(request):
-    return render(request,'accounts/register.html')
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request,'accounts/register.html')
 
 
 def logout(request):
